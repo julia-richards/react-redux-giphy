4d9fbfe725ebebac2832810d73f02c10c7a173ec
Aaron <aaron.hanson.brb@gmail.com>
2020-11-17 13:43:12 -0700

    Phase 2 Done

--- src/actions/gifActions.js
+++ src/actions/gifActions.js
@@ -1,7 +1,15 @@
 // TODO: Import all of your importing your API util function
+import fetchGifs from '../util/apiUtil';
 
 // TODO: Set and export a constant for your `RECEIVE_GIFS` action type
+export const RECEIVE_GIFS = 'RECEIVE_GIFS';
 
 // TODO: Write a function that returns your `action` object literal
+const receiveGifs = gifs => {
+  return {
+    type: RECEIVE_GIFS,
+    gifs
+  }
+};
 
 // TODO: Write a thunk action creator
--- src/reducers/gifsReducer.js
+++ src/reducers/gifsReducer.js
@@ -1,9 +1,15 @@
 // TODO: Import the `RECEIVE_GIFS` constant
+import RECEIVE_GIFS from '../actions/gifActions';
 
 const gifsReducer = (state = [], action) => {
   switch (action.type) {
     // TODO: Return the GIFs from the action object if the action type is `RECEIVE_GIFS`
+    case RECEIVE_GIFS:
+      return [...state, action.gifs]
+
     // TODO: Return the previous state by default
+    default:
+      return state;
   }
 };
 
